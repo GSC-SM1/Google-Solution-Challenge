cbbb6107a475158ea1cbac731d208c6517d85041
keemzleun <92363679+keemzleun@users.noreply.github.com>
2024-02-05 16:37:13 +0900

    Initial commit

---
+++ src/test/java/com/example/GSC/GscApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.GSC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GscApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
